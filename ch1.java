2063fcbc57cabf9020978f0527edabf2167f8b45
Chris Hyun Ho Kim <Chris@Chriss-MacBook-Pro.local>
2014-06-30 23:35:42 -0700

    ch1 progress
    todo

--- ch1.java
+++ ch1.java
@@ -48,11 +48,80 @@ public class ch1 {
     }
     return true;
   }
+
+  public static String replaceWhiteSpace(String str, int trueLen) {
+    char[] arr = str.toCharArray();
+    char[] result = new char[arr.length];
+    int len = arr.length;
+    for(int i=trueLen-1; i >=0; i--) {
+      if (arr[i] == ' ') {
+        result[--len] = '0';
+        result[--len] = '2';
+        result[--len] = '%';
+      } else {
+        result[--len] = arr[i];
+      }
+    }
+    return new String(result);
+  }
+
+  public static String compressString(String str) {
+    StringBuilder result = new StringBuilder();
+    char[] arr = str.toCharArray();
+    char current = arr[0];
+    int count = 1;
+    for (int i=1; i < arr.length; i++) {
+      if (arr[i] == current) { count++; }
+      else {
+        result.append(current);
+        result.append(count);
+        current = arr[i];
+        count = 1;
+      }
+    }
+    result.append(current);
+    result.append(count);
+    return result.toString();
+  }
+
+  public static boolean isSubstring(String str1, String str2) {
+    char[] arr1 = str1.toCharArray();
+    char[] arr2 = str2.toCharArray();
+    for (int i=0; i < arr1.length - arr2.length+1; i++) {
+      if ((arr1[i] == arr2[0]) && str1.substring(i, i+arr2.length).equals(new String(arr2))) {
+          return true;
+      }
+    }
+    return false;
+  }
+
+  public static boolean isRotation(String str1, String str2) {
+    if (str1.length() != str2.length()) { return false; }
+    return isSubstring(str1+str1, str2);
+  }
+
+
+
+
+  /* TODO: Write an algo. such that if an elem in an MxN matrix is 0, its entire row and col
+           are set to 0
+  */
+
+
+  /* TODO: Given an image represented by an NxN mat, where each pixel in the image is 4 bytes,
+           write a method to rotate the image by 90 degress.
+           Can you do this in plcae?
+  */
+
   
   public static void main (String[] args) {
     String s = "reverse this";
     System.out.println(isUniqueFast(s));
     System.out.println(rever(s));
     System.out.println(isPerm("abcdee", "debca"));
+    System.out.println(replaceWhiteSpace("Mr John Smith    ", 13));
+    System.out.println(compressString("aaabbccddeeee"));
+    System.out.println(isSubstring("wefcccchriislfwek", "chris"));
+    System.out.println(isRotation("chriskim", "imchrisk"));
   }
 }
